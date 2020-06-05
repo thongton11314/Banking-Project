c4de23e2fde5e63415dc0724416c095841173609
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2020-06-05 12:46:25 -0700

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -1,22 +1,13 @@
 #include "bst.h"
 
-// constructor
-// initialize the root
 BSTree::BSTree() {
     root = nullptr;
 }
 
-// deconstructor
-// delete every left and right subtree node
 BSTree::~BSTree() {
     makeEmpty();
 }
 
-// insert
-// insert new node to left or right subtree
-// call the insertHelper to perform traverse recursively
-// the passed in node will be taken and insert in tree
-// if the node is already in tree, then the passed in node will be deleted
 bool BSTree::insert(Client * newClient) {
 
     // if tree is empty, this is the first node of tree
@@ -32,9 +23,6 @@ bool BSTree::insert(Client * newClient) {
         return insertHelper(newClient, root);
 }
 
-// insertHelper
-// recursively traverse the tree
-// this function is sub-function of insert
 bool BSTree::insertHelper(Client * newClient, Node *& current) {
     // base case
     if (current == nullptr) {
@@ -59,18 +47,13 @@ bool BSTree::insertHelper(Client * newClient, Node *& current) {
     }
 }
 
-// retrieve
-// retrieve the node that match with target
-// call the retrieveHelper to perform traverse recursively
 bool BSTree::retrieve(const Client & target, Client *& ptr) const {
     
     // return true if retrieve successfully, else return false
     return retrieveHelper(target, ptr, root);
 }
 
-// retrieveHelper
-// recursively traverse the tree
-// this function is sub-function of retrieve
+
 bool BSTree::retrieveHelper(const Client & target, Client *& retriever, Node * current) const {
     
     // if not found target
@@ -96,18 +79,12 @@ bool BSTree::retrieveHelper(const Client & target, Client *& retriever, Node * c
     }
 }
 
-// display
-// display the node of tree from left to right, by acessding order
-// call the displayHelper to perform traverse recursively
 void BSTree::display() const {
 
     // display the tree from left to right
     displayHelper(root);
 }
 
-// displayHelper
-// recursively traverse the tree
-// this function is sub-function of display
 void BSTree::displayHelper(Node * current) const {
     
     // base case
@@ -122,9 +99,6 @@ void BSTree::displayHelper(Node * current) const {
     }
 }
 
-// makeEmpty
-// delete every node from the tree
-// this function will be called when deconstructor is called
 void BSTree::makeEmpty() {
 
     // early exit if root is null pointer
@@ -138,9 +112,6 @@ void BSTree::makeEmpty() {
     root = nullptr;
 }
 
-// makeEmptyHelper
-// recursively traverse the tree
-// this function is sub-function of makeEmpty
 void BSTree::makeEmptyHelper(Node *& current) {
     
     // delete every node
@@ -152,11 +123,6 @@ void BSTree::makeEmptyHelper(Node *& current) {
     }
 }
 
-// isEmpty
-// check if the tree has any node or not
 bool BSTree::isEmpty() const {
     return root == nullptr;
 }
-
-
-
