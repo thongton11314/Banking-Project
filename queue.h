ae8101e7a4dc3a052f012abe60ebbf6d7a9e0d97
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2020-06-05 12:51:55 -0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -30,24 +30,15 @@ public:
     ~Queue();                       // destructor
     void clear();                   // clear out queue
     bool isEmpty() const;           // is the queue empty?
-
-    // insert item, assume object passed in is valid, return whether successful
     bool enqueue(Transaction*);
-
-    // remove item, parameter is removed item, return whether item is removed 
     bool dequeue(Transaction*&);
-
-    // retrieve item, parameter is item at top, return whether item exists
     bool peek(Transaction*&) const;
 private:
     struct Node {
-        Transaction* data;          // pointer to actual data stored
+        Transaction* data;
         Node* next;
     };
-
-    // for linked list implementation
-    Node* front;                    // pointer to front of queue
-    Node* rear;                     // pointer to rear of queue
+    Node* front;
+    Node* rear;
 };
-#endif // !HISTORY_AND_TRANSACTION_CONTAINER
-
+#endif
