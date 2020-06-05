129827c6bb32d0c7230c8092987ac94f22d37b01
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2020-06-05 12:55:33 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,23 +6,11 @@ using namespace std;
 //---------------------------------------------------------------------------
 // Main
 int main() {
-
-    // instantiate manager class
     Manager bank;
-
-    // instantiate file stream for setting data
     ifstream clientFile("lab5data.txt"), 
         transactionFile("lab5command.txt");
-
-    // reads all client information from text file
     bank.buildClients(clientFile);
-
-    // reads all client command from text file
     bank.buildTransactions(transactionFile);
-
-    // process all transaction from file
     bank.processTransactions();
-    
-    //display history of client
     bank.report();
 }
