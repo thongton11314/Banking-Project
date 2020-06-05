5cb3373064afb075096ad209f27133ebc37479c2
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2020-06-05 12:52:36 -0700

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,37 +1,23 @@
 #include "queue.h"
 
-// constructor
-// initialize queue
 Queue::Queue() {
-
-    // front is rear at the begining
     front = rear = nullptr;
 }
 
-// deconstructor
-// deallocate memory
 Queue::~Queue() {
     clear();
 }
 
-// clear
-// delete each node carefully since allocate them
 void Queue::clear() {
 
-    // keep deleting if queue still has datas
-    while (front != nullptr) {
 
-        // using for next node
+    while (front != nullptr) {
         Node* temp = front;
         front = front->next;
-
-        // delete node
         delete temp->data;
         delete temp;
         temp = nullptr;
     }
-
-    // always set null pointer when done
     front = rear = nullptr;
 }
 
@@ -39,26 +25,20 @@ bool Queue::isEmpty() const {
     return (front == nullptr);
 }
 
-// enqueue
-// insert item into rear of queue
 bool Queue::enqueue(Transaction * dataptr) {
 
     Node* nodeptr = new Node;
     assert(nodeptr != nullptr);
     nodeptr->data = dataptr;
     nodeptr->next = nullptr;
-
-    // link at front if empty, otherwise at the rear
     if (isEmpty())
         front = nodeptr;
     else
         rear->next = nodeptr;
-    rear = nodeptr;                                // either way, rear is set
+    rear = nodeptr;  
     return true;
 }
 
-// dequeue
-// remove item from front of queue
 bool Queue::dequeue(Transaction *& dataptr) {
     if (isEmpty())
         return false;
@@ -67,14 +47,12 @@ bool Queue::dequeue(Transaction *& dataptr) {
     Node* nodeptr = front;
     front = front->next;
     if (isEmpty())
-        rear = nullptr;                   // if it's the last node in queue
+        rear = nullptr;       
     delete nodeptr;
     nodeptr = nullptr;
     return true;
 }
 
-// peak
-// retrieve item at front of queue
 bool Queue::peek(Transaction *& dataptr) const {
     if (isEmpty())
         return false;
