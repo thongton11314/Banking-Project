fa611c5a1425896403184c383678ce7c74cd0bdb
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2020-06-05 12:57:25 -0700

    Update manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "manager.h"
 
-// build client container
 void Manager::buildClients(ifstream & infile)
 {
     Client* ptr;
@@ -74,11 +73,6 @@ void Manager::processTransactions()
             // in case
             default:
                 break;
-
-            // error transaction
-            // another case other D,W,M is already 
-            // handle in buildTransaction
-            // when read transaction from file
         }
     }
    
@@ -86,8 +80,6 @@ void Manager::processTransactions()
     ptrTransaction = nullptr;
 }
 
-// this function will be called in processTransactions function
-// if move same client ID, then client has withdraw/deposit history
 void Manager::moveHelper(Transaction * ptrTran) {
 
     // pointer point to transaction data
@@ -138,75 +130,39 @@ void Manager::moveHelper(Transaction * ptrTran) {
     ptrDeliver = ptrReceiver = nullptr;
 }
 
-// this function will be called in processTransactions function
 void Manager::depositHelper(Transaction *ptrTran) {
-
-    // pointer that hold a client data
     Client* ptrClient;
-
-    // temporary target to check
     Client target;
-
-    // set temporary target data
     target.setId(ptrTran->getDeliveredClientID());
-
-    // check target in container
     bool check = clientContainer.retrieve(target, ptrClient);
-
-    // perform transaction
     if (check) {
         if (ptrClient->deposit(ptrTran->getDeliveredAccID()
             , ptrTran->getAmount())) {
-
-            // add transaction history into client
             ptrClient->addHistory(ptrTran);
         }
     }
-
-    // error transaction, unknown client
     else {
         ptrTran->setUnknownClient(true);
         invalidTransactionContainer.enqueue(ptrTran);
     }
-
-    // set data to null pointer after finished work
     ptrClient = nullptr;
 }
 
-// this function will be called in processTransactions function
 void Manager::withdrawHelper(Transaction *ptrTran) {
-    
-    // pointer that hold a client data
     Client* ptrClient;
-
-    // temporary target to check
     Client target;
-
-    // set temporary target data
     target.setId(ptrTran->getDeliveredClientID());
-
-    // check target in container
     bool check = clientContainer.retrieve(target, ptrClient);
-
-    // perform transaction
     if (check) {
-
-        // sufficient fund
         if (ptrClient->withdraw(ptrTran->getDeliveredAccID()
             , ptrTran->getAmount())) {
-            
-            // add transaction history into client
             ptrClient->addHistory(ptrTran);
         }
-
-        // unsufficient fund
         else {
             ptrTran->setSufficient(false);
             invalidTransactionContainer.enqueue(ptrTran);
         }
     }
-
-    // error transaction, unknown client
     else {
         ptrTran->setUnknownClient(false);
         invalidTransactionContainer.enqueue(ptrTran);
@@ -215,65 +171,37 @@ void Manager::withdrawHelper(Transaction *ptrTran) {
     ptrClient = nullptr;
 }
 
-// report
-// show all invalid transaction, or account unsufficient fund,
-// active clients have transaction, and summary report clients infor
 void Manager::report()
 {
-    // this function has leak memory
-    // error transaction
     invalidTransactions();
     cout << endl;
-
-    // report summary
     clientSummary();
     cout << endl;
-
-    // all client report
     clientsReport();
     cout << endl;
 }
 
-// display invalid transaction
-void Manager::invalidTransactions() {
-
-    // early exist
+void Manager::invalidTransactions() {  
     if (invalidTransactionContainer.isEmpty())
         return;
-
     cout << "Error:" << endl;
-
-    // pointer that hold an invalid transaction
     Transaction* ptrTransaction;
-    
-    // hold different type of error
     Queue invalidType;
     Queue insufficientFunds;
     Queue unknownClient;
-
-    // sort invalid transaction
     while (invalidTransactionContainer.dequeue(ptrTransaction)) {
-        
-        // get invalid type of transaction
         if (ptrTransaction->getTranType() != 'D'
             && ptrTransaction->getTranType() != 'W'
             && ptrTransaction->getTranType() != 'M') {
             invalidType.enqueue(ptrTransaction);
         }
-
-        // get insufficient transaction
         else if (ptrTransaction->isSufficient() == false) {
             insufficientFunds.enqueue(ptrTransaction);
         }
-        
-        // get unkown clients request
         else if (ptrTransaction->isUnknownClient() == true) {
             unknownClient.enqueue(ptrTransaction);
         }
     }
-
-    // print out error in order
-    // Invalid transaction
     if (!insufficientFunds.isEmpty()) {
     cout << "Invalid type of transaction:" << endl;
         while (invalidType.dequeue(ptrTransaction)) {
@@ -284,8 +212,6 @@ void Manager::invalidTransactions() {
                 delete ptrTransaction;
         }
     }
-
-    // Insufficient transfer funds
     if (!insufficientFunds.isEmpty()) {
         cout << "Insufficient transfer funds:" << endl;
         while (insufficientFunds.dequeue(ptrTransaction)) {
@@ -297,8 +223,6 @@ void Manager::invalidTransactions() {
             delete ptrTransaction;
         }
     }
-
-    // Unknown client request
     if (!unknownClient.isEmpty()) {
         cout << "Unknown client request:" << endl;
         while (unknownClient.dequeue(ptrTransaction)) {
@@ -321,17 +245,11 @@ void Manager::invalidTransactions() {
 
     ptrTransaction = nullptr;
 }
-
-// display client summary
-// this only show the current client that has transaction actively
 void Manager::clientSummary() {
     cout << "History of active clients:" << endl;
     cout << "In process" << endl;
 
 }
-
-// display clients report
-// show all summary of clients information and their accounts
 void Manager::clientsReport() {
     cout << "Clients summary:" << endl;
     
